d5d706be46f0f66f0d87fff8721b7f3b38727262
wangweiye <wwyknight@163.com>
2019-04-27 20:06:24 +0800

    fix

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -37,10 +37,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # 创建一颗子弹，并将其加入到编组bullets中
-        if (len(bullets) < ai_settings.bullets_allowed):
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
     """响应松开"""
@@ -48,3 +45,20 @@ def check_keyup_events(event, ship):
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
+
+def update_bullets(bullets):
+    """更新子弹的位置，并删除已消失的子弹"""
+    # 更新子弹的位置
+    bullets.update()
+
+    # 删除已消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom < 0:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """如果还没有达到限制，就发射一颗子弹"""
+    # 创建新子弹，并将其加入到编组bullets中
+    if (len(bullets) < ai_settings.bullets_allowed):
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
