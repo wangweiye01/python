047693aea1c69592ada238127c2dfc3fe0b2b0ad
wangweiye <wwyknight@163.com>
2019-07-09 19:18:10 +0800

    fix

--- matplotlib/scatter_squares.py
+++ matplotlib/scatter_squares.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=40, edgecolor='none', c=y_values, cmap=plt.cm.Blues)
+plt.scatter(x_values, y_values, s=40, edgecolor='none',
+            c=y_values, cmap=plt.cm.Blues)
 
 # 设置图标标题，并给坐标轴加上标签
 plt.title("Square Numbers", fontsize=24)
