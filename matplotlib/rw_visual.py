2d203fdf3e2840e73c6cfa7e528f99b32fb911d0
wangweiye <wwyknight@163.com>
2019-07-10 08:49:05 +0800

    fix

--- matplotlib/rw_visual.py
+++ matplotlib/rw_visual.py
@@ -8,7 +8,14 @@ from random_walk import RandomWalk
 while True:
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none')
+
+    # 突出起点和终点(重绘起点和终点)
+    plt.scatter(0, 0, c='green', edgecolor='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none', s=100)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
