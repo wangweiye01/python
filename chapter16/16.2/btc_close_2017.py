d55c64cf6ff76e0ae1eb02d64f1fe04f31f4bf75
wangweiye <wwyknight@163.com>
2019-07-11 15:26:37 +0800

    fix

--- chapter16/16.2/btc_close_2017.py
+++ chapter16/16.2/btc_close_2017.py
@@ -7,6 +7,7 @@ except ImportError:
     from urllib.request import urlopen
 import json
 import requests
+import pygal
 
 json_url = 'https://raw.githubusercontent.com/muxuezi/btc/master/btc_close_2017.json'
 req = requests.get(json_url)
@@ -16,11 +17,22 @@ with open('btc_close_2017_requests.json', 'w') as f:
 # 加载json格式
 file_requests = req.json()
 
+dates = []
+months = []
+weeks = []
+weekdays = []
+close = []
 for btc_dict in file_requests:
-    date = btc_dict['date']
-    month = btc_dict['month']
-    week = btc_dict['week']
-    weekday = btc_dict['weekday']
-    close = btc_dict['close']
-    print("{} is month {} week {}, {}, the close price is {} RMB".format(
-        date, month, week, weekday, close))
+    dates.append(btc_dict['date'])
+    months.append(int(btc_dict['month']))
+    weeks.append(int(btc_dict['week']))
+    weekdays.append(btc_dict['weekday'])
+    close.append(int(float(btc_dict['close'])))
+
+line_chart = pygal.Line(x_label_rotation=20, show_minor_x_labels=False)
+line_chart.title = '收盘价（￥）'
+line_chart.x_labels = dates
+N = 20 # x轴坐标每隔20天显示一次
+line_chart.x_labels_major = dates[::N]
+line_chart.add('收盘价', close)
+line_chart.render_to_file('收盘价折线图（￥）.svg')
