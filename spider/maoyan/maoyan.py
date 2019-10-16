0d8613a4776aa83d29d45dd8c7b836aec1805fdb
wangweiye <wwyknight@163.com>
2019-10-16 12:36:21 +0800

    fix

--- spider/maoyan/maoyan.py
+++ spider/maoyan/maoyan.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def get_one_page(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS XX 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36'
@@ -10,9 +11,11 @@ def get_one_page(url):
         return response.text
     return None
 
+
 def main():
     url = 'http://maoyan.com/board/4'
     html = get_one_page(url)
     print(html)
+
 
 main()
